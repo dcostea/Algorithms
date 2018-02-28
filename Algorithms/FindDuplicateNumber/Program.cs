bc54cdacd33e31c97c0d147c680f0bd0c127ad16
dcostea <daniel_costea@ymail.com>
2018-03-01 00:45:50 +0200

    Add htmls

---
+++ Algorithms/FindDuplicateNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+
+namespace FindDuplicateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 2, 8, 7, 5, 8, 4, 1, 9, 6 };
+
+            WriteLine("Initial array items: ");
+
+            Sort(arr);
+
+            var duplicate = FindDuplicate(arr);
+            WriteLine(duplicate == -1 ? "No duplicate found" : "Found " + duplicate);
+
+
+            ReadKey(true);
+        }
+
+        static int FindDuplicate(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == a[i + 1])
+                {
+                    return a[i];
+                }
+            }
+
+            return -1;
+        }
+
+        static void Sort(int[] a)
+        {
+            int aux;
+
+            for (int j = 0; j <= a.Length - 2; j++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        aux = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = aux;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/LeapYear/Program.cs
@@ -0,0 +1,64 @@
+﻿using static System.Console;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Leap years:");
+
+            const int START_YEAR = 2000;
+            const int END_YEAR = 2500;
+
+
+            var currentYear = System.DateTime.Now.Year;
+
+            for (int i = START_YEAR; i < END_YEAR; i++)
+            {
+                if (IsLeap(i))
+                {
+                    Write(i + " ");
+                }
+            }
+
+            WriteLine();
+            WriteLine();
+
+            var n = (END_YEAR - START_YEAR) / 4;
+            for (int i = 0; i <= n; i++)
+            {
+                if (IsLeapOptimized(i))
+                {
+                    Write(i * 4 + START_YEAR + " ");
+                }
+            }
+
+            ReadKey(true);
+        }
+
+        static bool IsLeap(int n)
+        {
+            if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsLeapOptimized(int n)
+        {
+            if ((n % 25 != 0) || (n % 100 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
